ccdc53aadfa2eb148e70d0cf551edc5573516090
pranjalisingh1201 <pranjalisingh1201@gmail.com>
2023-07-23 14:16:22 +0530

    @RequestParam Implementations.

--- src/main/java/com/springboot/MVC/HomeController.java
+++ src/main/java/com/springboot/MVC/HomeController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -17,7 +18,7 @@ public class HomeController {
 		return "index.jsp";//Telling Dispatcher Servlet that we need to call index.jsp --> Downloading the jsp page.
 	}
 	
-	@RequestMapping("add")
+	/*@RequestMapping("add")
 	public String add(HttpServletRequest req)
 	{
 		System.out.println("Add Controller is called.");
@@ -27,6 +28,17 @@ public class HomeController {
 		HttpSession session = req.getSession();//Set value through seesion and send back.
 		session.setAttribute("num3", num3);
 		return "result.jsp";//or return "result.jsp?num3"+num3;
+	}*/
+	
+	@RequestMapping("add")
+	public String add(@RequestParam("num1")int i,@RequestParam("num2")int j , HttpSession session)
+	{
+		//Same as we can pass HttpServletRequest object in parameter we can do the same for HTTP SESSION.
+		//@RequestParam will assign value getting from a parameter called num1 to i and num2 to j-- Fetching from Address Bar.
+		System.out.println("Add Controller2 is called.");
+		int num3 = i + j;
+		session.setAttribute("num3", num3);
+		return "result.jsp";
 	}
 
 }
